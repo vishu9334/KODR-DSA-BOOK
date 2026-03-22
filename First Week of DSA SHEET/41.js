fd76db10bc63641512d2c5b0b8309b3478f7c15c
vishal kumar <144935874+vishu9334@users.noreply.github.com>
2026-03-22 21:59:59 +0530

    This question complete

--- First Week of DSA SHEET/41.js
+++ First Week of DSA SHEET/41.js
@@ -1,13 +1,37 @@
-// // Make a choice based calculator using do while
+// Make a choice based calculator using do while
 
-// const calculator = (cal, input1 , input2 )=>{
+const calculator = (cal, input1 , input2 )=>{
        
-//     do {
-//         console.log("calculator")
-//     } while (
+    do {
+       switch (cal) {
+        case "add" :
+                const res = input1 + input2
+                console.log(res)
+            break;
        
-//     ){
-
-//     }
+        case "sub" :
+                const res1 = input1 - input2
+                console.log(res1)
+            break;
        
-// }
+        case "divide" :
+                const res2 = input1 / input2
+                console.log(res2)
+            break;
+       
+        case "multiplie" :
+                const res3 = input1 * input2
+                console.log(res3)
+            break;
+       
+        case "modlus" :
+                const res4 = input1 % input2
+                console.log(res4)
+            break;
+       
+        default:
+            break;
+       }
+    } while (false)
+}
+calculator("multiplie", 4, 5)
